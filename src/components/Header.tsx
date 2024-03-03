2144192b437e6a6363bd71a32a875adaf10dcdee
Casey Ocampo <caseysoflo@gmail.com>
2024-03-03 15:23:40 -0500

    chore: update text wrap and spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="max-w-4xl m-auto my-2">
       <h1 className="text-3xl sm:text-4xl font-bold text-amber-950">Investment Calculator</h1>
-      <p className="mb-12 text-amber-950">An investment calculator that helps calculate and view projected investments.</p>
+      <p className="mb-6 text-amber-950 text-balance sm:mb-12">An investment calculator that helps calculate and view projected investments.</p>
     </header>
   )
 }
