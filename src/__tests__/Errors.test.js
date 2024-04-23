696d033be28680ebc6b7dde986409b989b02abe3
Casey Ocampo <caseysoflo@gmail.com>
2024-04-23 09:15:01 -0400

    fix: update error test

--- src/__tests__/Errors.test.js
+++ src/__tests__/Errors.test.js
@@ -1,11 +1,11 @@
 import { render, screen } from "@testing-library/react";
-import Errors from "./Errors";
+import Errors from "../components/Errors";
 import "@testing-library/jest-dom";
 
 test("on empty starting amount submission, announce error message", async () => {
   render(<Errors />);
 
-  screen.debug();
+  //   screen.debug();
 
   const section = screen.getByLabelText("Error messages");
   expect(section).toHaveAttribute("aria-live", "assertive");
