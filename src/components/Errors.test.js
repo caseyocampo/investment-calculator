eae19bdd96eadd103a40c804664e2e9db90a5edb
Casey Ocampo <caseysoflo@gmail.com>
2024-04-24 09:27:22 -0400

    chore: add new test to support all error messages

--- src/components/Errors.test.js
+++ src/components/Errors.test.js
@@ -2,7 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Errors from "./Errors";
 import "@testing-library/jest-dom";
 
-test("on empty starting amount submission, populate error message", async () => {
+test("on empty starting amount form submission, populate error message", async () => {
   render(<Errors errorStartingAmount="true" />);
 
   const section = screen.getByLabelText("Error messages");
@@ -11,6 +11,20 @@ test("on empty starting amount submission, populate error message", async () => 
     .find(
       (listitem) =>
         listitem.textContent === "The Starting Amount field cannot be blank",
+    );
+  expect(errorMessage).toBeInTheDocument();
+});
+
+test("on starting amount form submission that contains letters, populate error message", async () => {
+  render(<Errors errorNoLetters="true" />);
+
+  const section = screen.getByLabelText("Error messages");
+  const errorMessage = screen
+    .getAllByRole("listitem")
+    .find(
+      (listitem) =>
+        listitem.textContent ===
+        "The Starting Amount field cannot contain letters",
     );
   expect(errorMessage).toBeInTheDocument();
 });
